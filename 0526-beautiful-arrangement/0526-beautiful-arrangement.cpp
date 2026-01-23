d8e0f09b5af8757e260a3e4540d923537b06fffb
Rahul A B <abr14022007@gmail.com>
2026-01-23 13:58:28 +0530

    Time: 47 ms (46.69%), Space: 7.9 MB (56.88%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-
-    int backtracking(int n,vector<bool>&visible,int pos){
-        if(n<pos){return 1;}
-        int count=0;
-        for(int i=1;i<=n;i++){
-            if(!visible[i] && (pos%i==0 || i%pos==0)){
-                visible[i]=true;
-                count+=backtracking(n,visible,pos+1);
-                visible[i]=false;
+    int beautiful(int n, vector<bool>& visible, int pos) {
+        if (n < pos) {
+            return 1;
+        }
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visible[i]&&( pos % i == 0 || i % pos == 0)) {
+                visible[i] = true;
+                count += beautiful(n, visible, pos + 1);
+                visible[i] = false;
             }
         }
         return count;
     }
     int countArrangement(int n) {
-        vector<bool>visible(n+1,false);
-        return backtracking(n,visible,1);
-        
+        vector<bool> vector(n, false);
+        return beautiful(n, vector, 1);
     }
 };
