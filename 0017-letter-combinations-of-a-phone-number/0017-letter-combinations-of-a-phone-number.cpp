cca7bdb5285242be48dd0398e045e3647ea2426e
Rahul A B <abr14022007@gmail.com>
2026-05-20 11:58:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.25%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-
-    void backtracking(string digits,string&path,vector<string>&result,vector<string>&mapping,int index){
-        if(index==digits.length()){
-            result.push_back(path);
-            return;
-        }
-        string letters=mapping[digits[index]-'0'];
-        for(char letter:letters){
-            path.push_back(letter);
-            backtracking(digits,path,result,mapping,index+1);
-            path.pop_back();
-        }
+void backtracking(string digits,string&path,vector<string>&result,vector<string>&board,int index){
+    if(index==digits.length()){
+        result.push_back(path);
+        return;
     }
+    string letters=board[digits[index]-'0'];
+    for(char letter:letters){
+        path.push_back(letter);
+        backtracking(digits,path,result,board,index+1);
+        path.pop_back();
+    }
+}
     vector<string> letterCombinations(string digits) {
-        if(digits.length()==0){
-            return {};
-        }
+        if(digits.length()==0)return {};
         string path;
         vector<string>result;
-        vector<string>mapping={
+        vector<string>board={
             "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
         };
-        backtracking(digits,path,result,mapping,0);
+        backtracking(digits,path,result,board,0);
         return result;
-        
     }
 };
